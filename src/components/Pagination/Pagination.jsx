f8a9fba2f4add13fd688882c665bd161fe72e1b9
Richard Palacios G <apalaciosg91@gmail.com>
2021-10-12 15:49:03 -0500

    feat(Pagination): add objetc props for pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({onPrevious, onNext}) => {
+
+	const handlePrevious = () => {
+		onPrevious();
+	}
+
+	consthandleNext = () => {
+		onNext();
+	}
+
 	return (
 		<nav aria-label="Page navigation example">
 			<ul class="pagination justify-content-center m-5">
-				<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+				<li class="page-item"><a class="page-link" href="#" onClick={handlePrevious}>Previous</a></li>
 				<li class="page-item"><a class="page-link" href="#">1</a></li>
 				<li class="page-item"><a class="page-link" href="#">2</a></li>
 				<li class="page-item"><a class="page-link" href="#">3</a></li>
-				<li class="page-item"><a class="page-link" href="#">Next</a></li>
+				<li class="page-item"><a class="page-link" href="#" onClick={handleNext}>Next</a></li>
 			</ul>
 		</nav>
 	)

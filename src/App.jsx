87bdc9cfd8a00741bfc4ab2aaa84aa472a55084f
Richard Palacios G <apalaciosg91@gmail.com>
2021-10-12 06:38:15 -0500

    feat: clean project and add bootstrap on index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-// import Container from '@mui/material/Container';
-// import Typography from '@mui/material/Typography';
-// import Box from '@mui/material/Box';
-// import Link from '@mui/material/Link';
 import Anuncios from './components/Users/User'
 
 function App() {

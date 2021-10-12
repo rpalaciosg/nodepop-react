12f2651b1338d034539601b4ea3d0c6a89e873ff
Richard Palacios G <apalaciosg91@gmail.com>
2021-10-12 14:55:09 -0500

    feat(Jumbotron): Add Jumbotron component

---
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Jumbotron = () => {
+	return (
+		 <div class="album py-5 bg-light">
+          <div class="container">
+            <section class="py-5 text-center container">
+              <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                  <h1 class="fw-light">Nodepop</h1>
+                  <p class="lead text-muted">No lo usas. Vendelo!</p>
+                  <p>
+                    <a href="#" class="btn btn-primary my-2">
+                      Crea un anuncio aquí
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+		)
+}
+
+	<div>
+			
+		</div>
+
+export default Jumbotron
